db42667ccf3032cbd91fbcd539bacda8e2a9be37
Hanah Betsy Robertson <hanah@giga1.bioinformatik.uni-saarland.de>
2022-05-22 15:51:08 +0200

    added path to input files

--- Scripts/0_PreProcessing/featureCounts.py
+++ Scripts/0_PreProcessing/featureCounts.py
@@ -4,20 +4,20 @@ import json
 if __name__ == "__main__":
 
     with open('paths.json') as f:
-            d = json.load(f)
+        d = json.load(f)
 
-    bam_files = d['ChIPSeq files']
+    bam_files = d['RNASeq files']
     ref = d['Reference genome']
     tissue1 = d["tissue1"]
     tissue2 = d["tissue2"]
     threads = d['threads']
 
     currdir = os.getcwd()
-    opdir = currdir + '0_Files/' + tissue1+'_'+tissue2+'_counts'
+    opdir = currdir + '/0_Files/' + tissue1+'_'+tissue2+'_counts'
 
     # -T -> specify cores
     # -a -> required option for specifying path to GTF
     # -o -> required option for specifying path to, and name of the text output (count matrix)
 
 
-    os.system('featureCounts -p -T '+ threads +' -a ' + ref + ' -o ' + opdir +' *.bam')
+    os.system('featureCounts -p -T '+ threads +' -a ' + ref + ' -o ' + opdir + ' ' + bam_files+ '/*.bam')
