d2dd5aba1553ecf88c4ead2aff4d0ead879b8c25
Hanah <hanahbetsy25@gmail.com>
2022-05-22 11:04:34 +0200

    flank reference 50 -200 bp

--- Scripts/0_PreProcessing/prepare_FlanksRef.py
+++ Scripts/0_PreProcessing/prepare_FlanksRef.py
@@ -4,32 +4,28 @@ import json
 with open('paths.json') as f:
     d = json.load(f)
 
-
 ref = d['Reference genome']
-
-
-ref = pd.read_csv(ref,
-                  delimiter='\t', skiprows=7, header=None)
+ref = pd.read_csv(ref, delimiter='\t', skiprows=7, header=None)
 ref = ref[ref[2] == 'exon']
 
-# ref['diff_start'] = ref[3].diff()
-# ref['diff_stop'] = ref[4].diff()
-# diff = [1, -1]
-# ref = ref[~((ref['diff_start'].isin(diff)) & (ref['diff_stop'].isin(diff)))]
+flanks = [50, 100, 200]
 
-start_df = ref.drop(ref.columns[4], 1)
-stop_df = ref.drop(ref.columns[3], 1)
+for flank in flanks:
 
-start_df[4] = pd.to_numeric(start_df[3]) + 200  # extend 200bp 5' direction
-start_df[3] = pd.to_numeric(start_df[3]) - 200  # extend 200bp 3' direction
+    start_df = ref.drop(ref.columns[4], 1)
+    stop_df = ref.drop(ref.columns[3], 1)
 
-stop_df[3] = pd.to_numeric(stop_df[4]) - 200  # extend 200bp 5' direction
-stop_df[4] = pd.to_numeric(stop_df[4]) + 200  # extend 200bp 3' direction
+    start_df[4] = pd.to_numeric(start_df[3]) + flank  # extend 200bp 5' direction
+    start_df[3] = pd.to_numeric(start_df[3]) - flank  # extend 200bp 3' direction
 
-new_ref = pd.concat([start_df, stop_df]).sort_index(kind='merge')
+    stop_df[3] = pd.to_numeric(stop_df[4]) - flank  # extend 200bp 5' direction
+    stop_df[4] = pd.to_numeric(stop_df[4]) + flank  # extend 200bp 3' direction
 
-new_ref[3] = new_ref[3].astype(int)
-new_ref[4] = new_ref[4].astype(int)
+    new_ref = pd.concat([start_df, stop_df]).sort_index(kind='merge')
 
-new_ref[[0, 3, 4]].to_csv('0_Files/flanks' + str(200) + '.bed',
-                          sep='\t', index=False, header=False)
+    new_ref[3] = new_ref[3].astype(int)
+    new_ref[4] = new_ref[4].astype(int)
+
+    new_ref[[0, 3, 4]].to_csv('0_Files/flanks' + str(flank) + '.bed', sep='\t', index=False, header=False)
+
+
