77688c75673833756224a5e83536041796e72874
Hanah <hanahbetsy25@gmail.com>
2022-05-22 11:22:13 +0200

    dPSI annotation 50,100,200 flanks

--- Scripts/1_MAJIQ/post-bedtools.py
+++ Scripts/1_MAJIQ/post-bedtools.py
@@ -1,33 +1,56 @@
 import pandas as pd
 
+
+flank_lens = [50, 100, 200]
 junctions = pd.read_csv('0_Files/majiq_junctions.csv', delimiter='\t')
-flanks = pd.read_csv('0_Files/majiq_flanks.bed', delimiter='\t', header=None)
 
-# drop flanks that have no junction
-flanks = flanks[flanks[4] != -1]
+annot = []
+for length in flank_lens:
+        flanks = pd.read_csv('0_Files/majiq_flanks' + str(length) + '.bed', delimiter='\t', header=None)
 
-# merge the flanks df with the jns df
-flanks[6] = flanks[[1, 2]].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
-flanks.drop([3, 5],  axis=1, inplace=True)
-flanks.set_axis(['seqid', 'start', 'stop', 'junction0', 'flanks'], axis=1, inplace=True)
 
-junctions['index'] = junctions.index
-flank_jns = pd.merge(junctions, flanks, on=['junction0', 'seqid'])
-flank_jns.sort_values(['index'], inplace=True)
-flank_jns = flank_jns.reset_index(drop=True)
-del(flank_jns['index'])
-flank_jns['mean_dpsi_per_lsv_junction'] = pd.to_numeric(flank_jns['mean_dpsi_per_lsv_junction'])
+        # drop flanks that have no junction
+        flanks = flanks[flanks[4] != -1]
 
-# # get all junctions that belong to each flank
-flank_jns_group = flank_jns.groupby(['seqid', 'flanks'])['mean_dpsi_per_lsv_junction']\
-        .apply(lambda val: ','.join(str(v) for v in val)).reset_index()
+        # adjust flank length to 200bp +-
+        if length < 200:
+                flanks[1] -= (200 - length)
+                flanks[2] += (200 - length)
+
+        # merge the flanks df with the jns df
+        flanks[6] = flanks[[1, 2]].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+        flanks.drop([3, 5], axis=1, inplace=True)
+
+        flanks.set_axis(['seqid', 'start', 'stop', 'junction0', 'flanks'], axis=1, inplace=True)
+
+        junctions['index'] = junctions.index
+        flank_jns = pd.merge(junctions, flanks, on=['junction0', 'seqid'])
+
+        flank_jns.sort_values(['index'], inplace=True)
+        flank_jns = flank_jns.reset_index(drop=True)
+        del (flank_jns['index'])
+        flank_jns['dpsi_'+str(length)] = pd.to_numeric(flank_jns['mean_dpsi_per_lsv_junction'])
+
+        # # get all junctions that belong to each flank
+        flank_jns_group = flank_jns.groupby(['flanks'])['dpsi_'+str(length)] \
+                .apply(lambda val: ','.join(str(v) for v in val)).reset_index()
+        annot.append(flank_jns_group)
+
+
+for a in annot:
+        a.set_index('flanks',inplace=True)
+
+df = pd.concat(annot,axis=1,sort=False).reset_index()
+df.dpsi_50.fillna(df.dpsi_100, inplace=True)
+df.dpsi_50.fillna(df.dpsi_200, inplace=True)
+flank_jns_group = df.drop(['dpsi_100', 'dpsi_200'], axis=1)
+flank_jns_group.columns = ['flanks', 'mean_dpsi_per_lsv_junction']
 
 # FILTER 3: if flank has 1+ junctions, keep junction with highest dPSI value
 flank_jns_group['max_dPSI'] = flank_jns_group['mean_dpsi_per_lsv_junction'].str.split(',')\
         .apply(lambda x: max(map(float, x)))  # string -> list of strings -> list of floats -> max float
 
-
-# get the corresponding junction for each flank's max dPSI value
+# # get the corresponding junction for each flank's max dPSI value
 flank_jns_group = pd.merge(flank_jns_group[['flanks', 'max_dPSI']], flank_jns, on=['flanks'], how='inner')
 flank_jns_group = flank_jns_group[flank_jns_group['mean_dpsi_per_lsv_junction'] == flank_jns_group['max_dPSI']]
 
@@ -38,7 +61,6 @@ flank_jns_group.drop_duplicates(subset='flanks', keep='first', inplace=True)
 del(flank_jns_group['max_dPSI'])
 flank_jns_group = flank_jns_group[['gene_id', 'lsv_id', 'seqid', 'junction0', 'mean_dpsi_per_lsv_junction',
         'probability_changing', 'flanks', 'start', 'stop', 'strand']]
-
 
 # Get all filtered flanks
 flank_jns_group.drop_duplicates().to_csv('0_Files/all_flanks.csv', sep='\t', index=False)
