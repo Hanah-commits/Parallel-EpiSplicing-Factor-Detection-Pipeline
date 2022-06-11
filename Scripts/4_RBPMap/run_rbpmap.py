676a085be4ace92186e544ca9ca4da83f8521f93
Hanah <hanahbetsy25@gmail.com>
2022-06-11 20:46:37 +0200

    changed input path

--- Scripts/4_RBPMap/run_rbpmap.py
+++ Scripts/4_RBPMap/run_rbpmap.py
@@ -21,7 +21,7 @@ with open('paths.json') as f:
 rbp_path = d["RBPmap directory"]
 
 currdir = os.getcwd()
-file = os.getcwd() + '/0_Files/input.txt'
+file = os.getcwd() + '/../RBPmap/input.txt'
 os.chdir(rbp_path)
 
 sfs = ['BRUNOL4', 'BRUNOL5', 'BRUNOL6', 'DAZAP1', 'ESRP2', 'FMR1', 'FUS', 'FXR1', 'FXR2', 'HNRNPA1', 'HNRNPA1L2', 'HNRNPA2B1', 'HNRNPC', 'HNRNPF', 'HNRNPH1', 'HNRNPH2', 'HNRNPK', 'HNRNPL', 'HNRNPM', 'HNRNPU', 'HuR', 'KHDRBS1', 'KHDRBS2', 'KHDRBS3', 'MBNL1', 'PABPC1', 'PABPN1', 'PCBP1', 'PCBP2', 'PTBP1', 'QKI', 'RALY', 'RBFOX1', 'RBM24', 'RBM28', 'RBM3', 'RBM4', 'RBM42', 'RBM5', 'RBM8A', 'SART3', 'SFPQ', 'SNRNP70', 'SNRPA', 'SRSF1', 'SRSF10', 'SRSF2', 'SRSF7', 'SRSF9', 'TARDBP', 'TIA1', 'U2AF2', 'YBX1', 'ZC3H10', 'ZCRB1', 'ZNF638']
