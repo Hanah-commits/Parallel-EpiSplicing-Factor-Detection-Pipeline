28a211b8e1a172939cf71a259c7cb9a0012c914c
Hanah-commits <57112634+Hanah-commits@users.noreply.github.com>
2022-06-27 20:43:32 +0200

    stratified k-fold

--- Scripts/5_Classification/classifier.py
+++ Scripts/5_Classification/classifier.py
@@ -7,6 +7,10 @@ from sklearn.utils import resample
 from sklearn import metrics
 import matplotlib.pyplot as plt
 from sklearn.metrics import RocCurveDisplay
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve
+from collections import defaultdict
+from functools import reduce
 
 
 def classifier(output_dir, method='LinearRegression', predictors='all', alpha=1.0, l1_ratio=0.5, folds=5):
@@ -94,5 +98,74 @@ def classifier(output_dir, method='LinearRegression', predictors='all', alpha=1.
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
+def stratified_classifier(output_dir):
+
+    features = pd.read_csv('0_Files/all_features.csv', delimiter='\t')
+
+
+    sf = ['BRUNOL4', 'BRUNOL5', 'BRUNOL6', 'DAZAP1', 'ESRP2', 'FMR1', 'FUS', 'FXR1', 'FXR2', 'HNRNPA1', 'HNRNPA1L2', 'HNRNPA2B1', 'HNRNPC', 'HNRNPF', 'HNRNPH1', 'HNRNPH2', 'HNRNPK', 'HNRNPL', 'HNRNPM', 'HNRNPU', 'HuR', 'KHDRBS1', 'KHDRBS2', 'KHDRBS3', 'MBNL1', 'PABPC1', 'PABPN1', 'PCBP1', 'PCBP2', 'PTBP1', 'QKI', 'RALY', 'RBFOX1', 'RBM24', 'RBM28', 'RBM3', 'RBM4', 'RBM42', 'RBM5', 'RBM8A', 'SART3', 'SFPQ', 'SNRNP70', 'SNRPA', 'SRSF1', 'SRSF10', 'SRSF2', 'SRSF7', 'SRSF9', 'TARDBP', 'TIA1', 'U2AF2', 'YBX1', 'ZC3H10', 'ZCRB1', 'ZNF638']
+
+    sf_data = features[sf]
+    features['label'] = features['label'].map({'epigene': 1, 'non-epigene': 0}).astype(int)
+    X, y = sf_data.values, features['label'].values
+
+    clf = RandomForestClassifier(n_estimators=100, criterion='gini')
+    kf = StratifiedKFold(n_splits=10)
+
+    tprs = []
+    aucs= []
+    base_fpr = np.linspace(0, 1, 101)
+
+    plt.figure(figsize=(5, 5))
+    plt.axes().set_aspect('equal', 'datalim')
+    gini_scores = []
+
+    for i, (train, test) in enumerate(kf.split(X, y)):
+        model = clf.fit(X[train], y[train])
+        y_score = model.predict_proba(X[test])
+        fpr, tpr, _ = roc_curve(y[test], y_score[:, 1])
+        roc_auc = metrics.auc(fpr, tpr)
+        aucs.append(roc_auc)
+    
+        plt.plot(fpr, tpr, 'b', alpha=0.15)
+        tpr = np.interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+        gini_scores.append(dict(zip(sf,model.feature_importances_)))
+
+    
+    # mean gini_impurity across all folds
+    def foo(r, d):
+        for k in d:
+            r[k].append(d[k])
+    gini_scores = reduce(lambda r, d: foo(r, d) or r, gini_scores, defaultdict(list))
+    gini_scores = pd.DataFrame(gini_scores)
+    mean_gini = gini_scores.mean(axis=0)
+    print('Gini Scores: ')
+    print(mean_gini)
+    
+
+    tprs = np.array(tprs)
+    mean_tprs = tprs.mean(axis=0)
+    std = tprs.std(axis=0)
+    aucs = np.array(aucs)
+    mean_auc = aucs.mean(axis=0)
+
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+
+    plt.plot(base_fpr, mean_tprs, 'b', label = 'AUC = %0.2f' % mean_auc)
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.3)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title="Receiver Operating Characteristic"
+    plt.savefig(output_dir +'ROC.png')
+
+
 if __name__ == "__main__":
-    classifier(predictors='sf', output_dir=sys.argv[1])
+    # classifier(predictors='sf', output_dir=sys.argv[1])
+    stratified_classifier(output_dir=sys.argv[1])
