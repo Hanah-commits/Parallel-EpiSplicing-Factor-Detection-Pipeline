2a96fea30a6515ad4449c64d44a074adcfe916ae
Hanah <hanahbetsy25@gmail.com>
2023-04-03 14:05:36 +0200

    prepare flanks from exon bed coords

--- Scripts/PreProcessing/prepare_FlanksRef.py
+++ Scripts/PreProcessing/prepare_FlanksRef.py
@@ -5,27 +5,28 @@ with open('paths.json') as f:
     d = json.load(f)
 
 ref = d['Reference genome']
-ref = pd.read_csv(ref, delimiter='\t', skiprows=7, header=None)
-ref = ref[ref[2] == 'exon']
+
+ref = pd.read_csv(ref, delimiter='\t',header=None)[[0,1,2]]
 
 flanks = [50, 100, 200]
 
 for flank in flanks:
 
-    start_df = ref.drop(ref.columns[4], 1)
-    stop_df = ref.drop(ref.columns[3], 1)
+    start_df = ref.drop(ref.columns[2], 1)
+    stop_df = ref.drop(ref.columns[1], 1)
 
-    start_df[4] = pd.to_numeric(start_df[3]) + flank  # extend 200bp 5' direction
-    start_df[3] = pd.to_numeric(start_df[3]) - flank  # extend 200bp 3' direction
 
-    stop_df[3] = pd.to_numeric(stop_df[4]) - flank  # extend 200bp 5' direction
-    stop_df[4] = pd.to_numeric(stop_df[4]) + flank  # extend 200bp 3' direction
+    start_df[2] = pd.to_numeric(start_df[1]) + flank  # extend 200bp 5' direction
+    start_df[1] = pd.to_numeric(start_df[1]) - flank  # extend 200bp 3' direction
+
+
+    stop_df[1] = pd.to_numeric(stop_df[2]) - flank  # extend 200bp 5' direction
+    stop_df[2] = pd.to_numeric(stop_df[2]) + flank  # extend 200bp 3' direction
 
     new_ref = pd.concat([start_df, stop_df]).sort_index(kind='merge')
 
-    new_ref[3] = new_ref[3].astype(int)
-    new_ref[4] = new_ref[4].astype(int)
+    new_ref[1] = new_ref[1].astype(int)
+    new_ref[2] = new_ref[2].astype(int)
 
-    new_ref[[0, 3, 4]].to_csv('0_Files/flanks' + str(flank) + '.bed', sep='\t', index=False, header=False)
-
-
+    print(new_ref)
+    new_ref.to_csv('0_Files/flanks' + str(flank) + '.bed', sep='\t', index=False, header=False)
