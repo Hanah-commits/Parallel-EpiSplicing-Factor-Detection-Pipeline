2ad2de0cc0d11fd8c19ee653f9258728bd4f4fa5
Hanah <hanahbetsy25@gmail.com>
2023-04-05 17:20:24 +0200

    fixed path bug

--- Scripts/PreProcessing/prepare_FlanksRef.py
+++ Scripts/PreProcessing/prepare_FlanksRef.py
@@ -21,18 +21,18 @@ flanks = ["50", "100", "200"]
 for flank in flanks:
 
     # exon boundary external flanks
-    os.system("bedtools flank -i exon_coords.bed -g " + ref_genome + " -b "+ flank + " > 0_Files/flanks.bed" )
+    os.system("bedtools flank -i 0_Files/exon_coords.bed -g " + ref_genome + " -b "+ flank + " > 0_Files/flanks.bed" )
 
     # separate start,stop flank coords
-    os.system("sed -n 'n;p' flanks.bed > 0_Files/stop.bed")
-    os.system("sed -n 'p;n' flanks.bed > 0_Files/start.bed")
+    os.system("sed -n 'n;p' 0_Files/flanks.bed > 0_Files/stop.bed")
+    os.system("sed -n 'p;n' 0_Files/flanks.bed > 0_Files/start.bed")
 
     # exon boundary internal flanks
-    os.system("bedtools slop -i start.bed -g " + ref_genome + " -l 0 -r " + flank + " > 0_Files/start_flanks.bed")
-    os.system("bedtools slop -i stop.bed -g " + ref_genome +" -l " + flank + " -r 0 > 0_Files/stop_flanks.bed")
+    os.system("bedtools slop -i 0_Files/start.bed -g " + ref_genome + " -l 0 -r " + flank + " > 0_Files/start_flanks.bed")
+    os.system("bedtools slop -i 0_Files/stop.bed -g " + ref_genome +" -l " + flank + " -r 0 > 0_Files/stop_flanks.bed")
 
     # combine start,stop flank coords
-    os.system("paste -d'\n' start_flanks.bed stop_flanks.bed > 0_Files/flanks" + flank + ".bed")
+    os.system("paste -d'\n' 0_Files/start_flanks.bed 0_Files/stop_flanks.bed > 0_Files/flanks" + flank + ".bed")
 
     # remove intermediate files
     os.system("rm 0_Files/start*.bed")
