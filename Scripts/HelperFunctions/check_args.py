ba79a87aa7cebeb2b290841607db5eac1bc38a9c
Marina Shustova <marina.shustova@xored.com>
2023-08-24 11:25:16 +0200

    changes in config file, master script, check_args function

--- Scripts/HelperFunctions/check_args.py
+++ Scripts/HelperFunctions/check_args.py
@@ -10,82 +10,88 @@ def check_args():
     with open('paths.json') as f:
         d = json.load(f)
 
-    args = {
-    "RNASeq files" : d['RNASeq files'],
-    "Reference genome" : d['Reference genome'],
-    "Reference fasta": d['Reference fasta'],
-    "tissue1" : d["tissue1"],
-    "tissue2" : d["tissue2"],
-    "Histone modifications" : d["Histone modifications"],
-    "ChIPSeq files" : d["ChIPSeq files"],
-    "MAJIQ config" : d["MAJIQ config"],
-    "RBPmap directory" : d["RBPmap directory"],
-    "threads" : d['threads']
-    }
+    procs = d["list_of_processes"]
+    output_dirs = []
 
-    #check histone modifications & tissue names
-    args["Histone modifications"] = [x for x in args["Histone modifications"] if x]
-    if len(args["Histone modifications"]) == 0:
-        raise ValueError('No Histone Modifications')
+    for proc in procs:
 
-    if len(args["tissue1"].strip()) == 0 or len(args["tissue2"].strip()) == 0:
-        raise ValueError('No Tissue Name(s)')
+        args = {
+        "RNASeq files" : d['RNASeq files'],
+        "Reference genome" : d['Reference genome'],
+        "Reference fasta": d['Reference fasta'],
+        "tissue1" : d["tissue1"],
+        "tissue2" : d["tissue2"],
+        "Histone modifications" : d["Histone modifications"],
+        "ChIPSeq files" : d["ChIPSeq files"],
+        "MAJIQ config" : d["MAJIQ config"],
+        "RBPmap directory" : d["RBPmap directory"],
+        "threads" : d['threads']
+        }
+
+        #check histone modifications & tissue names
+        args["Histone modifications"] = [x for x in args["Histone modifications"] if x]
+        if len(args["Histone modifications"]) == 0:
+            raise ValueError('No Histone Modifications')
+
+        if len(args["tissue1"].strip()) == 0 or len(args["tissue2"].strip()) == 0:
+            raise ValueError('No Tissue Name(s)')
         
-    #check datatypes
-    try:
-        int(args["threads"])
-    except:
-        raise ValueError('Invalid Input ' + args["threads"])
+        #check datatypes
+        try:
+            int(args["threads"])
+        except:
+            raise ValueError('Invalid Input ' + args["threads"])
 
 
-    # check path validity of directories
-    dirs = ["RNASeq files", "ChIPSeq files", "RBPmap directory"]
-    dir_paths = []
-    for dir in dirs:
+        # check path validity of directories
+        dirs = ["RNASeq files", "ChIPSeq files", "RBPmap directory"]
+        dir_paths = []
+        for dir in dirs:
 
-        if args[dir][-1] != '/':
-            args[dir] += '/'
+            if args[dir][-1] != '/':
+                args[dir] += '/'
 
-        dir_paths.append(args[dir])
+            dir_paths.append(args[dir])
         
 
-    for path in dir_paths:
+        for path in dir_paths:
         
-        if not os.path.exists(os.path.dirname(path)):
-            raise ValueError('Path does not exist ' + path)
+            if not os.path.exists(os.path.dirname(path)):
+                raise ValueError('Path does not exist ' + path)
 
-    # check path validity of files
-    file_paths = [args["Reference genome"], args["Reference fasta"], args["MAJIQ config"]]
-    for file in file_paths:
+        # check path validity of files
+        file_paths = [args["Reference genome"], args["Reference fasta"], args["MAJIQ config"]]
+        for file in file_paths:
         
-        if not os.path.isfile(file):
-            raise ValueError('File does not exist ' + file)
+            if not os.path.isfile(file):
+                raise ValueError('File does not exist ' + file)
 
         
-    # check if temp directories already exist
-    temp_dirs = ['0_Files/', '../RBPmap/']
-    for dir in temp_dirs:
-        if os.path.exists(dir):
-            # temp dir not empty
-            if len(os.listdir(dir)) != 0:
-                raise ValueError('Delete or move directory to another location ' + dir)
-        else: 
-            #create temp dir
-            Path(dir).mkdir(parents=True, exist_ok=True)
+        # check if temp directories already exist
+        temp_dirs = ['0_Files/', '../RBPmap/']
+        for dir in temp_dirs:
+            if os.path.exists(dir):
+                # temp dir not empty
+                if len(os.listdir(dir)) != 0:
+                    raise ValueError('Delete or move directory to another location ' + dir)
+            else: 
+                #create temp dir
+                Path(dir).mkdir(parents=True, exist_ok=True)
 
 
-    # create custome output directory tissue1_tissue2_timestamp
-    output_dir = str(Path(os.getcwd()).parent.absolute()) + "/Output/"+ args["tissue1"]+ "_" + args["tissue2"]+ "_" + str(time.time()) +"/"
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
+        # create custome output directory process_tissue1_tissue2_timestamp
+        output_dir = str(Path(os.getcwd()).parent.absolute()) + "/Output/"+ proc+ args["tissue1"]+ "_" + args["tissue2"]+ "_" + str(time.time()) +"/"
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+        output_dirs.append(output_dir)
 
 
-    with open('paths.json', 'w') as fp:
-        json.dump(args, fp)
+        with open('paths.json', 'w') as fp:
+            json.dump(args, fp)
 
-    # copy input arguments (paths.json) to output_dir
-    shutil.copyfile('paths.json', output_dir+'paths.json')
+        # copy input arguments (paths.json) to output_dir
+        shutil.copyfile('paths.json', output_dir+'paths.json')
 
-    return output_dir
+    return procs, output_dirs
 
 
 def move_dirs(output_dir):
--- Scripts/master.py
+++ Scripts/master.py
@@ -2,169 +2,186 @@ import os
 import sys
 from HelperFunctions.check_args import check_args, move_dirs
 
+from multiprocessing import Process
+
 # Check input arguments from paths.json
-output_dir = check_args()
+list_of_processes, output_dirs = check_args()
 
-#check command line arguments
-weights = False
-if len(sys.argv) > 1 and sys.argv[1] == "-w":
-    weights = True
+# The function that will be executed by all processes (semantics is not changed)
+def master_function(proc, output_dir):
 
-# STEP 0: Preprocessing
+    #check command line arguments
+    weights = False
+    if len(sys.argv) > 1 and sys.argv[1] == "-w":
+        weights = True
 
-# Differential Expression Analysis
-if weights:
+    # STEP 0: Preprocessing
+
+    # Differential Expression Analysis
+    if weights:
+        try:
+            exec(open("PreProcessing/featureCounts.py").read())
+            os.system("Rscript PreProcessing/Limma.R")
+        except Exception as ex:
+            print(ex)
+            move_dirs(output_dir)
+            sys.exit(1)
+
+    # Prepare flank reference : 50, 100, 200 bp
     try:
-        exec(open("PreProcessing/featureCounts.py").read())
-        os.system("Rscript PreProcessing/Limma.R")
+        os.system("python PreProcessing/prepare_FlanksRef.py")
     except Exception as ex:
         print(ex)
         move_dirs(output_dir)
         sys.exit(1)
 
-# Prepare flank reference : 50, 100, 200 bp
-try:
-    os.system("python PreProcessing/prepare_FlanksRef.py")
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 1: Execute MAJIQ - Differential Exon Usage
-try:
-    os.system("python 1_MAJIQ/runMAJIQ.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 2: Execute MANorm -  Differential Histone Modifications
-try:
-    os.system("python 2_MANorm/manorm_all.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 3: Process MAJIQ output
-try:
-    os.system("python 1_MAJIQ/post-MAJIQ.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 4: BEDTools - Annotate exon flanks with MAJIQ junctions
-try:
-    exec(open("1_MAJIQ/annotate-MAJIQ.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 5: Process BEDTools output
-try:
-    exec(open("1_MAJIQ/post-bedtools.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 6: BEDTools - Annotate exon flanks with MANorm peaks
-try:
-    os.system('python 2_MANorm/annotate-MANorm.py ' + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 7: Process peak-annotated flanks
-try:
-    os.system("python 2_MANorm/post-manorm.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 8: DEU - DHM Correlation
-try:
-    exec(open("3_Episplicing/correlation.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 9: Prepare RBPmap input
-try:
-    exec(open("4_RBPMap/pre-rbp.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 10: Execute RBPmap
-try:
-    exec(open("4_RBPMap/run_rbpmap.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 11: Process RBPMap output
-try:
-    exec(open("4_RBPMap/post-rbp.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-try:    
-    exec(open("5_Classification/rbp_pvals.py").read())
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
-
-# STEP 12: Add logFC weights to binding scores from RBPMap
-if weights:
+    # STEP 1: Execute MAJIQ - Differential Exon Usage
     try:
-        exec(open("PreProcessing/get_weights.py").read())
-        exec(open("4_RBPMap/rbp-weights.py").read())
+        os.system("python 1_MAJIQ/runMAJIQ.py " + output_dir)
     except Exception as ex:
         print(ex)
         move_dirs(output_dir)
         sys.exit(1)
 
-# STEP 13: Prep Feature Matrix
-try:
-    os.system("python 5_Classification/features.py " + output_dir + " " + str(weights))
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
+    # STEP 2: Execute MANorm -  Differential Histone Modifications
+    try:
+        os.system("python 2_MANorm/manorm_all.py " + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
 
-try: 
-    os.system("python 5_Classification/classifier_features.py " + output_dir + " " + str(weights))
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
+    # STEP 3: Process MAJIQ output
+    try:
+        os.system("python 1_MAJIQ/post-MAJIQ.py " + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
 
-# STEP 14: Binary Classification
-try:
-    os.system("python 5_Classification/classifier.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
+    # STEP 4: BEDTools - Annotate exon flanks with MAJIQ junctions
+    try:
+        exec(open("1_MAJIQ/annotate-MAJIQ.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
 
-# STEP 15: Enrichment
-try:
-    os.system("python 6_Enrichment/enrichment.py " + output_dir)
-except Exception as ex:
-    print(ex)
-    move_dirs(output_dir)
-    sys.exit(1)
+    # STEP 5: Process BEDTools output
+    try:
+        exec(open("1_MAJIQ/post-bedtools.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
 
-# STEP 15: Move files generated from current pipeline run to
-move_dirs(output_dir)
+    # STEP 6: BEDTools - Annotate exon flanks with MANorm peaks
+    try:
+        os.system('python 2_MANorm/annotate-MANorm.py ' + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 7: Process peak-annotated flanks
+    try:
+        os.system("python 2_MANorm/post-manorm.py " + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 8: DEU - DHM Correlation
+    try:
+        exec(open("3_Episplicing/correlation.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 9: Prepare RBPmap input
+    try:
+        exec(open("4_RBPMap/pre-rbp.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 10: Execute RBPmap
+    try:
+        exec(open("4_RBPMap/run_rbpmap.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 11: Process RBPMap output
+    try:
+        exec(open("4_RBPMap/post-rbp.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    try:    
+        exec(open("5_Classification/rbp_pvals.py").read())
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 12: Add logFC weights to binding scores from RBPMap
+    if weights:
+        try:
+            exec(open("PreProcessing/get_weights.py").read())
+            exec(open("4_RBPMap/rbp-weights.py").read())
+        except Exception as ex:
+            print(ex)
+            move_dirs(output_dir)
+            sys.exit(1)
+
+    # STEP 13: Prep Feature Matrix
+    try:
+        os.system("python 5_Classification/features.py " + output_dir + " " + str(weights))
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    try: 
+        os.system("python 5_Classification/classifier_features.py " + output_dir + " " + str(weights))
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 14: Binary Classification
+    try:
+        os.system("python 5_Classification/classifier.py " + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 15: Enrichment
+    try:
+        os.system("python 6_Enrichment/enrichment.py " + output_dir)
+    except Exception as ex:
+        print(ex)
+        move_dirs(output_dir)
+        sys.exit(1)
+
+    # STEP 15: Move files generated from current pipeline run to
+    move_dirs(output_dir)
+
+active_processes = []
+
+# Start the processes
+for pr, out_dir in zip(list_of_processes, output_dirs):
+    proc = Process(target=master_function, args=(pr, out_dir,))
+    active_processes.append(proc)
+    proc.start()
+
+# Wait for the processes to complete and free resources
+for proc in active_processes:
+        proc.join()
